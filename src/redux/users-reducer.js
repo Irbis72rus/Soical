d745091dda49f9517f1e0ac0da7a91782d2e878e
Геннадий <genabakaldin1995@mail.ru>
2021-05-23 20:36:27 +0500

    55. Сделали вывод всех страниц и переход при клике по ним

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,6 +1,8 @@
 const FOLLOW = 'ADD-FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 let initialState = {
     users: [
@@ -10,7 +12,10 @@ let initialState = {
         //     followed: true, fillName: 'Sasha', status: 'I am a boss too', location: {city:'Miscow', country: 'Russia'} },
         // { id: 3, photoUrl: 'https://media.istockphoto.com/photos/lion-in-high-grass-picture-id494856046?k=6&m=494856046&s=612x612&w=0&h=Eb3uv75Vld5c_n22v8i5XktUPl60czynY4EVx7XMuV4=',
         //     followed: false, fillName: 'Andrew', status: 'I am a boss too', location: {city:'Kiev', country: 'Ukraine'} }
-    ]
+    ],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 2
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -39,9 +44,22 @@ const usersReducer = (state = initialState, action) => {
             case SET_USERS: {
                 return {
                     ...state,
-                    users: [...state.users, ...action.users]
+                    users: action.users
                 }
             }
+            case SET_CURRENT_PAGE: {
+                return {
+                    ...state,
+                    currentPage: action.currentPage
+                }
+            }
+            case SET_TOTAL_USERS_COUNT: {
+                return {
+                    ...state,
+                    totalUsersCount: action.count
+                }
+            }
+            
         default:
             return state;
     }
@@ -51,5 +69,7 @@ const usersReducer = (state = initialState, action) => {
 export const followAC = (userId) => ({ type: FOLLOW, userId })
 export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId })
 export const setUsersAC = (users) => ({ type: SET_USERS, users })
+export const setCurrentPageAC = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage })
+export const setUsersTotalCountAC = (totalUsersCount) => ({ type: SET_TOTAL_USERS_COUNT, count: totalUsersCount})
 
 export default usersReducer;
